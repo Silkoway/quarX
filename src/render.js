f463894fff0dee4203d47a6177e705f76e9d93a7
Silkoway <ethancai920@gmail.com>
2022-04-18 14:15:20 +0800

    open folder support

--- src/render.js
+++ src/render.js
@@ -10,11 +10,13 @@ const fs = require('fs');
 const { app } = require('electron/main');
 const { ipcRenderer } = require('electron')
 
+var selected = null;
+
 const convertCSSToJSON = (str) => {
     var csslist = str.split('\n')
     var newlist = [];
     csslist.forEach(line => {
-        newlist.push({name: line.split(': ')[0], value: line.split(': ')[1]})
+        newlist.push({ name: line.split(': ')[0], value: line.split(': ')[1] })
     })
     return JSON.stringify(newlist);
 }
@@ -40,8 +42,8 @@ finally {
 const openPreferences = () => {
     console.log('dsfasef')
     ipcRenderer.send('openPreferences', '')
-    
-    
+
+
 }
 const preferences = () => {
     JSON.parse(fs.readFileSync(remoteapp.getPath('appData') + '/preferences.json', 'utf8')).forEach(thing => {
@@ -53,10 +55,7 @@ document.querySelector('#preferences').addEventListener('click', openPreferences
 
 document.querySelector('#textinput')?.addEventListener('input', () => {
 
-    inputtext = document.querySelector('#textinput')?.value ?? ''
     if (curfile !== '') document.title = `quarX - *${curfile}`
-    document.querySelector('#file-explorer').innerHTML = ''
-    document.querySelector('#file-explorer').innerHTML = `<div class="file">*${curfile}</div>`
 })
 
 document.querySelector('#readwrite').addEventListener('change', () => {
@@ -102,51 +101,95 @@ const openFile = () => {
     })
 
 }
-const openSingleFolder = (fileObj) => {
-    
-        fs.readdir(fileObj.filePaths[0], (err, files) => {
-            if (err) {
-                throw err;
-            }
-        
-            // files object contains all files names
-            // log them on console
-            document.querySelector('#file-explorer').innerHTML = ''
-            files.forEach(file => {
-                inputtext = fs.readFileSync(file, 'utf8').toString()
-                document.querySelector('#textinput.other').style = ''
-                document.querySelector('#textinput.document').style = 'display: none'
-                document.querySelector('#textinput.other').value = inputtext
-                curfile = file.replace(/^.*[\\\/]/, '')
-                if (file === curfile) {
-                    curfiledir = file
-                    document.title = `quarX - ${curfile}`
-                
-                    document.querySelector('#file-explorer').innerHTML += `<div class="file">${curfile}</div>`
-                    console.log(file);
-                } else {
-                    openSingleFolder(fileObj.filePaths[0] + file)
-                }
-                
-            });
-        });
-    
+
+const dree = require("dree");
+
+function parseTree(tree, currentDir) {
+    if (tree.type === "directory") {
+        const name = tree.name;
+        const result = [name];
+        for (let child of tree.children) {
+            parseTree(child, result);
+        }
+        result.push([]);
+
+        if (currentDir === undefined) {
+            return result;
+        } else {
+            currentDir.push(result);
+        }
+    }
+    if (tree.type === 'file') {
+        currentDir.push(tree.name)
+    }
 }
 const openFolder = async () => {
     `<button type="button" class="collapsible file">Folder</button>
     <div class="content">
       fsdgsfgsdfggdf
     </div>`
-    dialog.showOpenDialog({ properties: ['openDirectory'] }).then (fileObj => {
-    openSingleFolder(fileObj)
-})
-}   
+    dialog.showOpenDialog({ properties: ['openDirectory'] }).then(fileObj => {
+        const result = parseTree(dree.scan(fileObj.filePaths[0]))
+        document.querySelector('#file-explorer').innerHTML = ''
+        var layer = 0;
+        function check(result, path) {
+            var index = 0;
+            const curlayer = layer;
+            var end = ''
+            result.forEach(file => {
+                if (index === 0) {
+                    end += `<button type="button" class="collapsible file" style="width: calc(20vw - ${2 + layer}rem); padding-left: ${layer + 1}rem;">${file} <span class="arrow"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 16 16">
+                    <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+                    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+                  </svg></span></button><div class="content">`
+                } else {
+                    if (Array.isArray(file)) {
+                        layer = curlayer + 1;
+                        end += check(file, `${path}/${file}`)
+                    } else {
+                        end += `<div class="file" style="width: calc(20vw - ${3 + layer}rem); padding-left: ${layer + 2}rem;" filepath="${path}/${file}">${file}</div>`
+                    }
+                }
+                index++;
+            })
+            end += "</div>"
+
+            if (curlayer === 0) {
+                document.querySelector('#file-explorer').innerHTML += end
+            }   else {
+                return end
+            }
+           
+
+        }
+
+        check(result, fileObj.filePaths[0])
+
+        collapsible()
+
+        document.querySelectorAll('.file:not(button)').forEach(element => {
+            element.addEventListener('click', () => {
+                if (element === selected) return;
+                if (selected !== null) selected.classList = selected?.classList?.remove('selected')
+                document.querySelector('#textinput.other').style = ''
+                document.querySelector('#textinput.document').style = 'display: none'
+                filepath = element.getAttribute('filepath')
+                inputtext = fs.readFileSync(filepath, 'utf8')
+                document.querySelector('#textinput.other').value = inputtext
+                curfile = filepath.replace(/^.*[\\\/]/, '')
+                curfiledir = filepath
+                document.title = `quarX - ${curfile}`
+                element?.classList.add("selected")
+                selected = element
+            })
+        })
+
+    })
+}
 const saveFile = () => {
     if (curfile) {
         document.title = `quarX - ${curfile}`
         fs.writeFileSync(curfiledir, inputtext)
-        document.querySelector('#file-explorer').innerHTML = ''
-        document.querySelector('#file-explorer').innerHTML = `<div class="file">${curfile}</div>`
     }
 }
 
@@ -189,7 +232,7 @@ document.addEventListener('keydown', e => {
             document.querySelector('#textinput.document').innerHTML = parse(inputtext)
         }
         if (mode === 'write') {
-            
+
             document.querySelector('#textinput.other').style = ''
             document.querySelector('#textinput.document').style = 'display: none'
             document.querySelector('#textinput.other').value = inputtext
@@ -219,17 +262,41 @@ const minimise = () => {
 
 document.querySelector('#closebtn').addEventListener('click', close)
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+function addToggle() {
     this.classList.toggle("active");
+    if (this.classList.contains("active")) {
+        this.children[0].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-up-square" viewBox="0 0 16 16">
+    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+    <path d="M3.544 10.705A.5.5 0 0 0 4 11h8a.5.5 0 0 0 .374-.832l-4-4.5a.5.5 0 0 0-.748 0l-4 4.5a.5.5 0 0 0-.082.537z"/>
+  </svg>`
+    } else {
+        this.children[0].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-square" viewBox="0 0 16 16">
+        <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
+        <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+      </svg>`
+    }
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
-      content.style.display = "none";
+        content.style.display = "none";
     } else {
-      content.style.display = "block";
+        content.style.display = "block";
     }
-  });
 }
+
+function collapsible() {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].removeEventListener("click", addToggle);
+        coll[i].addEventListener("click", addToggle);
+    }
+}
+
+function tick() {
+    
+}
+
+
+
+
